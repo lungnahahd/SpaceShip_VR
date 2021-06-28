5f045565d5e80dc70bd32cf0571859a2b44b954c
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-06-28 15:37:30 +0900

    우주선 이동 기능 부여

---
+++ Assets/spaceShipMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceShipMovement : MonoBehaviour
+{
+    public GameObject Head;
+    public GameObject Cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Head.transform.Translate(Cam.transform.forward * Time.deltaTime);
+    }
+}
