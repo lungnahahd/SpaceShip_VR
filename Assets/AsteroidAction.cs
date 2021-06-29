ab9c542b93e07e1f0df3f9e9c3438cd66ecf2cf2
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-06-30 01:35:41 +0900

    소행성 제거 구현

---
+++ Assets/AsteroidAction.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidAction : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other){
+        Destroy(this.gameObject);
+    }
+}
