c5a8d49c71cb2b8375bd3458b7437187c98ab83f
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-06-29 23:53:06 +0900

    Script 좌우 움직임 부여)

--- Assets/spaceShipMovement.cs
+++ Assets/spaceShipMovement.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class spaceShipMovement : MonoBehaviour
 {
-    public GameObject Head;
-    public GameObject Cam;
+    public GameObject Head; // 우주선을 가르키는 변수
+    public GameObject Cam; // 카메라를 가르키는 변수
+
+    float CurrRot; // 현재 회전 값
+    float PrevRot; // 이전 회전 값
+    float DeltRot; // 현재 회전 값과 이전 회전 값의 차이를 구해서 양/음 에 따라 어느 방향으로 회전시킬지를 결정
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +21,13 @@ public class spaceShipMovement : MonoBehaviour
     void Update()
     {
         Head.transform.Translate(Cam.transform.forward * Time.deltaTime);
+        moverl();
+    }
+
+    void moverl()
+    {
+        CurrRot = Cam.tranform.eulerAngles.y;
+        DeltRot = CurrRot - PrevRot;
+        PrevRot = CurrRot;
     }
 }
