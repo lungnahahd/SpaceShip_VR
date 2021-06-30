5a4f3b9d42f085a44b3e8da665ffc97e982c44c1
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-06-30 18:47:14 +0900

    배경선택 Script

---
+++ Assets/SelectBack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectBack : MonoBehaviour
+{
+    public void OceanBack(){
+        PlayerPrefs.SetInt("back",1);
+        SceneManager.LoadScene("InGame");
+    }
+    public void MilkyBack(){
+        PlayerPrefs.SetInt("back",2);
+        SceneManager.LoadScene("InGame");
+    }
+}
