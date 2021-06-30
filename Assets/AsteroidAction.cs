c28d5c60c37e36b7b15ec6879c19ce6509e06777
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-06-30 17:47:47 +0900

    소행성 파괴 효과 및 소행성 배치

--- Assets/AsteroidAction.cs
+++ Assets/AsteroidAction.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class AsteroidAction : MonoBehaviour
 {
+    public GameObject ExpObj;
+    public GameObject Show;
     private void OnTriggerEnter(Collider other){
+        ExpObj.SetActive(true); // 폭발 활성화
+        Destroy(Show);
+        Invoke("RealDestroy",2);
+    }
+
+    void RealDestroy(){
         Destroy(this.gameObject);
     }
+
 }
