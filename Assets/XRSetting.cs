9754f65927bf78d3ea84444fc671b126ad627d89
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-06-30 21:07:20 +0900

    SpaceShipGame Ver1

---
+++ Assets/XRSetting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class XRSetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SwitchToVR());   
+    }
+
+    // Call via `StartCoroutine(SwitchToVR())` from your code. Or, use
+    // `yield SwitchToVR()` if calling from inside another coroutine.
+    IEnumerator SwitchToVR()
+    {
+        // Device names are lowercase, as returned by `XRSettings.supportedDevices`.
+        string desiredDevice = "cardboard"; // Or "cardboard".
+
+        // Some VR Devices do not support reloading when already active, see
+        // https://docs.unity3d.com/ScriptReference/XR.XRSettings.LoadDeviceByName.html
+        if (String.Compare(XRSettings.loadedDeviceName, desiredDevice, true) != 0)
+        {
+            XRSettings.LoadDeviceByName(desiredDevice);
+
+            // Must wait one frame after calling `XRSettings.LoadDeviceByName()`.
+            yield return null;
+        }
+
+        // Now it's ok to enable VR mode.
+        XRSettings.enabled = true;
+    }
+}
